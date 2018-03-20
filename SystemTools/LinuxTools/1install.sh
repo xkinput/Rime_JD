2c2787a8096492cae1c98da6833417f388047e50
倾书 <nmlixa@163.com>
2018-03-20 19:42:00 +0800

    工具：完善Linux部署工具，添加一笔两笔部署工具。添加星空系列配置文件。

--- SystemTools/LinuxTools/1install.sh
+++ SystemTools/LinuxTools/1install.sh
@@ -40,11 +40,9 @@ Get_SysPack_Name(){
 Get_SysPack_Name
 sleep 3
 clear
-NAME=../rime/Linux/name.txt
 echo "==========================================="
 echo "欢迎使用Linux_Rime输入法Fcitx-rime引导程序"
 echo "==========================================="
-echo "                 `cat $NAME`"
 echo "向导："
 echo "请在有网络环境下开始，否则请立即Ctrl+c停止！"
 echo "若在无网络情况下卸载输入法，将无法安装输入法"
@@ -56,20 +54,8 @@ echo "4、注销或重启系统。"
 echo "*过程可能较慢，请耐心等待。"
 echo "                          shell by Qshu"
 echo "==========================================="
-echo "*请阅读向导，8秒后开始安装……"
-if [ ! -d "../rime" ]; then
-	clear
-	echo "==========================================="
-	echo ""
-	echo "            未找到rime文件夹！"
-	echo ""
-	echo ""
-	echo "请下载rime文件放至安装器目录再继续安装。"
-	echo "==========================================="
-	sleep 5
-	exit
-fi
-sleep 8
+echo "*请阅读向导，5秒后开始安装……"
+sleep 5
 clear
 echo "==========================================="
 echo "欢迎使用Linux_Rime输入法Fcitx-rime引导程序"
@@ -112,36 +98,95 @@ echo "欢迎使用LinuxRime输入法Fcitx-rime引导程序"
 echo "==========================================="
 echo "第三步 复制Rime用户配置文件"
 echo "==========================================="
-rm -rf ~/.config/fcitx/rime
+JDL=""
+YB=""
+LB=""
+fcitx=~/.config/fcitx
+rime=~/.config/fcitx/rime
+if [ ! -d "./备份" ]; then
+	clear
+	mkdir 备份
+else
+	rm -rf 备份/*
+fi
+
+if [ ! -f "$rime/default.yaml" ]; then
+	echo "无需备份"
+else
+	cp -rf "$rime"/* ./备份/
+	echo "备份“用户文件夹”到“备份”文件夹	完成"
+fi
+sleep 1
+rm -rf "$rime"
 echo "清空配置文件目录					完成"
-cp -r ../../rime ~/.config/fcitx
-echo "复制Rime用户码表文件				完成"
-cp -r ../rime/*.yaml ~/.config/fcitx/rime
-echo "复制Rime用户配置文件				完成"
-echo "复制所有文件						完成"
-echo "==========================================="
 clear
-echo "预览 ~/.config/fcitx/rime/"
-echo "==========================================="
-ls ~/.config/fcitx/rime/
-echo "==========================================="
+echo "检测到键道6词库，是否同时安装？ y/n"
+read isInstall
+if [ "$isInstall" = "y" ] || [ "$isInstall" = "Y" ]; then
+	cp -rf ../../rime "$fcitx"
+	echo "复制Rime用户码表文件				完成"
+	cp -rf ../rime/*.yaml "$rime"
+	echo "复制Rime用户配置文件				完成"
+	echo "复制所有文件					完成"
+	JDL="键道6"
+else
+	echo "已拒绝安装。"
+fi
+sleep 1
+rime_xklb="../../../rime_xklb";
+if [ ! -d "$rime_xklb" ]; then
+	echo "没有两笔词库，跳过。"
+else
+	echo "检测到两笔词库，是否同时安装？ y/n"
+	read isInstall
+	if [ "$isInstall" = "y" ] || [ "$isInstall" = "Y" ]; then
+		cp -rf "$rime_xklb/rime" "$fcitx"
+		echo "复制码表完成					完成"
+		echo "复制所有文件					完成"
+		LB="星空两笔"
+	else
+		echo "已拒绝安装。"
+	fi
+fi
+sleep 1
+rime_xkyb="../../../rime_xkyb";
+if [ ! -d "$rime_xkyb" ]; then
+	echo "没有一笔词库，跳过。"
+else
+	echo "检测到一笔词库，是否同时安装？ y/n"
+	read isInstall
+	if [ "$isInstall" = "y" ] || [ "$isInstall" = "Y" ]; then
+		cp -rf "$rime_xkyb/rime" "$fcitx"
+		echo "复制码表完成					完成"
+		echo "复制所有文件					完成"
+		YB="星空一笔"
+	else
+		echo "已拒绝安装。"
+	fi
+fi
+if [ ! -d "$rime" ]; then
+	echo ""
+else
+	cp -rf ../*.yaml "$rime"
+	echo "全局配置文件						完成"
+fi
 sleep 1
 clear
+echo "预览 $rime/"
 echo "==========================================="
-echo "已完成安装"
+ls "$rime"
 echo "==========================================="
-sleep 1
+sleep 2
 clear
 echo "==========================================="
-echo "欢迎使用LinuxRime输入法Fcitx-rime引导程序"
+echo "已安装方案： $JDL $YB $LB"
+echo "请在fcitx重新部署后再尝试使用。"
 echo "==========================================="
+sleep 2
+clear
 echo " 注销 或 重启 系统后即可使用"
 echo "==========================================="
-echo "是否重启系统？"
-sleep 1
-echo "8秒后将重启系统，若不想重启系统请Ctrl + C停止"
+echo "8秒后重启，若不想重启系统请Ctrl+C，并且手动注销"
 echo "==========================================="
-sleep 1
-echo "已完成安装"
 sleep 8
 sudo reboot
