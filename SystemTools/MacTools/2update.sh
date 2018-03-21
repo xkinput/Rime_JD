d191e821a43f371cae0d658b498b646f7862e64a
岳飞飞 <yueh33@qq.com>
2018-03-21 11:38:31 +0800

    修正：Mac 升级工具

--- SystemTools/MacTools/2update.sh
+++ SystemTools/MacTools/2update.sh
@@ -1,51 +1,55 @@
 #!/bin/bash
 
-rime_xkjd="../.."
-rime_xklb="../../../rime_xklb"
-rime_xkyb="../../../rime_xkyb"
+xkjd="../../../Rime_JD"
+xklb="../../../rime_xklb"
+xkyb="../../../rime_xkyb"
 rime="$HOME/Library/Rime"
+bak="./备份"
+
 JDL=""
 YB=""
 LB=""
 
-if_schema (){
-# $1: filepath
-# $2: name
+count=0
+
+isHaveDict (){
+# $1: schema name
+# $2: filepath
 # $3: rime_xk..
-    if [ ! -e $1 ]; then
-    	echo "没有$2词库，跳过。"
+    if [ ! -e $2 ]; then
+    	echo "没有$1词库，跳过。"
     else
-    	echo "检测到$2词库，是否同时安装？ y/n"
+    	echo "检测到$1词库，是否同时安装？ y/n"
     	read isInstall
     	if [[ "$isInstall" == [yY] ]]; then
-    		cp -rf "$3/rime" $rime
+    		cp -rf $2/rime/*.yaml $rime
     		echo "复制码表完成					完成"
     		echo "复制所有文件					完成"
-		return  3
+		count=$(expr $count + 1)
+		return  1
     	else
     		echo "已拒绝安装。"
-		return  4
+		return  0
     	fi
     fi
-    sleep 1
-  }
+}
 
-if_user_dict() {
-# $1: xkjd6 / xkyb / xklb
-# $2: 键道6 / 两笔 / 一笔
-    if [ ! -f "./备份/$1.user.dict.yaml" ]; then
-    	echo "没有备份$2用户词库跳过。"
+isHaveUserDict() {
+# $1: 键道6 / 两笔 / 一笔
+# $2: xkjd6 / xkyb / xklb
+    if [ ! -f $bak/$2 ]; then
+	echo "· 没有备份$1用户词库跳过。"
     else
-    	cp -rf ./备份/$1.user.dict.yaml $rime
-    	echo "还原$2用户词库					完成"
+	cp -rf $bak/$2 "$rime"
+	echo "· 还原$1用户词库					完成"
     fi
-    sleep 2
-  }
+}
 
 # ====================================================================
 clear
 
-if [ ! -d "./备份" ]; then
+# 检测备份目录
+if [ ! -d $bak ]; then
 	clear
 	mkdir 备份
 else
@@ -53,49 +57,41 @@ else
 fi
 
 if [ ! -f "$rime/default.yaml" ]; then
-	echo "无需备份"
+	echo "· 未发现$rime无需备份"
 else
 	cp -rf $rime/* ./备份/
-	echo "备份“用户文件夹”到“备份”文件夹	完成"
+	echo "· 备份“$rime”到“$bak”文件夹	完成"
 fi
 
 sleep 1
-rm -rf "$rime"
-echo "清空配置文件目录					完成"
+
+#清空Rime目录
+if [ ! -d $rime ]; then
+	mkdir $rime
+else
+	rm -rf $rime
+	mkdir $rime
+fi
 clear
 
-if [ ! -d "./备份" ]; then
-	clear
-	mkdir 备份
-else
-	rm -rf 备份/*
+isHaveDict "键道6" $xkjd
+if [ $? == 1 ]; then
+    cp -rf ../rime/*.yaml $rime/
+    JDL="键道6"
 fi
 
-if [ ! -f "$rime/default.yaml" ]; then
-	echo "无需备份"
-else
-	cp -rf "$rime"/* ./备份/
-	echo "备份“用户文件夹”到“备份”文件夹	完成"
+isHaveDict "一笔" $xkyb
+if [ $? == 1 ]; then
+    YB="一笔"
 fi
+
+isHaveDict "两笔" $xklb
+if [ $? == 1 ]; then
+	LB="两笔"
+fi
+
 sleep 1
-rm -rf "$rime"
-echo "清空配置文件目录					完成"
 clear
-
-if_schema $rime_xkjd "键道6" "rime_xkjd"  
-if [ $? == 3 ];then
-  JDL="键道6"
-fi
-
-if_schema $rime_xkyb "一笔"  "rime_xkyb"  
-if [ $? == 3 ];then
-  YB="一笔"
-fi
-
-if_schema $rime_xklb "两笔"  "rime_xklb"  
-if [ $? == 3 ];then
-  LB="两笔"
-fi
 
 if [ ! -f "$rime" ]; then
 	echo ""
@@ -104,21 +100,24 @@ else
 	echo "全局配置文件						完成"
 fi
 
-if_user_dict "xkjd6" "键道6"
-if_user_dict "xkyb"  "一笔"
-if_user_dict "xklb"  "两笔"
+isHaveUserDict "键道6" "xkjd6.user.dict.yaml"
+isHaveUserDict "一笔" "xkyb.user.dict.yaml"
+isHaveUserDict "两笔" "xklb.user.dict.yaml"
 
 clear
 echo "预览 $rime/"
 echo "==========================================="
-ls "$rime"
+if [ ! -d $rime ]; then
+	echo "未发现$rime"
+else
+	ls "$rime"
+fi
 echo "==========================================="
 sleep 2
 clear
 echo "==========================================="
-echo "已安装方案： $JDL $YB $LB"
-echo "请重新部署后再尝试使用。"
+echo "已安装方案：$count个 $JD $YB $LB"
+echo "请在fcitx重新部署后再尝试使用。"
 echo "==========================================="
 sleep 1
-
 exit
