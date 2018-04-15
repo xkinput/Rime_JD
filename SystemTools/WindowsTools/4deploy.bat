4875739dac53ca9e8fe2ce5fdfc0fa5021fab41c
倾书 <nmlixa@163.com>
2018-04-15 18:08:16 +0800

    update Windows Tools

--- SystemTools/WindowsTools/4deploy.bat
+++ SystemTools/WindowsTools/4deploy.bat
@@ -6,8 +6,10 @@ title 小狼毫重新部署工具
 echo 正在尝试自动部署！
 echo 请稍等...
 "C:\Program Files (x86)\Rime\weasel-0.9.30\WeaselDeployer.exe" /deploy
+"C:\Program Files (x86)\Rime\weasel-0.10.0\WeaselDeployer.exe" /deploy
 cls
 echo 部署完成！
 echo 可以关闭我了
 "C:\Program Files (x86)\Rime\weasel-0.9.30\WeaselServer.exe"
+"C:\Program Files (x86)\Rime\weasel-0.10.0\WeaselServer.exe"
 exit
