dd6941e6e5ccb13ec26e62c500e272c612109d94
Qshu <nmlixa@163.com>
2018-04-29 15:14:14 +0800

    fix windowsTools

--- SystemTools/WindowsTools/1install.bat
+++ SystemTools/WindowsTools/1install.bat
@@ -6,6 +6,7 @@ echo 1、安装小狼毫输入法与小狼毫输入法扩展程序
 echo 注意：请不要改动安装地址，程序会在后期调用自动部署，改动则会失败
 app\weasel-0.9.30-installer.exe
 app\weasel-0.10.0.0-installer.exe
+app\weasel-0.11.1.0-installer
 app\weasel-expansion-0.9.18.0.exe
 echo .
 echo ======================
