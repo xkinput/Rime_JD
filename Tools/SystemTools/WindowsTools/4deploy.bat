4df971859f2e23dc13fd55534c3e13475fcbbe7c
unknown <>
2018-05-03 15:53:47 +0800

    修改安装升级工具路径

--- Tools/SystemTools/WindowsTools/4deploy.bat
+++ Tools/SystemTools/WindowsTools/4deploy.bat
@@ -5,11 +5,9 @@ title 小狼毫重新部署工具
 
 echo 正在尝试自动部署！
 echo 请稍等...
-"C:\Program Files (x86)\Rime\weasel-0.10.0\WeaselDeployer.exe" /deploy
 "C:\Program Files (x86)\Rime\weasel-0.11.1\WeaselDeployer.exe" /deploy
 cls
 echo 部署完成！
 echo 可以关闭我了
-"C:\Program Files (x86)\Rime\weasel-0.10.0\WeaselServer.exe"
 "C:\Program Files (x86)\Rime\weasel-0.11.1\WeaselServer.exe"
 exit
