f83d7e5c3658db89d50799d59a80f20c66b40f2c
倾书 <nmlixa@163.com>
2018-06-24 16:35:22 +0800

    fix TermsTools

--- Tools/TermsTools/1jdSortHeader_Mac.sh
+++ Tools/TermsTools/1jdSortHeader_Mac.sh
@@ -7,14 +7,14 @@ cp -rf ../../rime/xkjd6.wxw.dict.yaml ../BackDict/
 git add ../BackDict/*
 git commit -m "更新：备份旧词库"
 echo 开始处理排序
-sort -t $'\t' -k 2 词组.txt > xkjd6.cizu.dict.yaml
-sort -t $'\t' -k 2 单字.txt > xkjd6.danzi.dict.yaml
-sort -t $'\t' -k 2 英文.txt > xkjd6.yingwen.dict.yaml
-sort -t $'\t' -k 2 五二五.txt > xkjd6.wxw.dict.yaml
-cp -rf xkjd6.cizu.dict.yaml ./词组.txt
-cp -rf xkjd6.danzi.dict.yaml ./单字.txt
-cp -rf xkjd6.yingwen.dict.yaml ./英文.txt
-cp -rf xkjd6.wxw.dict.yaml ./五二五.txt
+sort -t $'\t' -k 2 cizu.txt > xkjd6.cizu.dict.yaml
+sort -t $'\t' -k 2 danzi.txt > xkjd6.danzi.dict.yaml
+sort -t $'\t' -k 2 yingwen.txt > xkjd6.yingwen.dict.yaml
+sort -t $'\t' -k 2 wxw.txt > xkjd6.wxw.dict.yaml
+cp -rf xkjd6.cizu.dict.yaml ./cizu.txt
+cp -rf xkjd6.danzi.dict.yaml ./danzi.txt
+cp -rf xkjd6.yingwen.dict.yaml .yingwen..txt
+cp -rf xkjd6.wxw.dict.yaml ./wxw.txt
 echo 更为当前文件
 sed -i '' -E $'1 i\\\n---\\\nname: xkjd6.cizu\\\nversion: "Q1"\\\nsort: original\\\n...\\\n' xkjd6.cizu.dict.yaml
 sed -i '' -E $'1 i\\\n---\\\nname: xkjd6.danzi\\\nversion: "Q1"\\\nsort: original\\\n...\\\n' xkjd6.danzi.dict.yaml
