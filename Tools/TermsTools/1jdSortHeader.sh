e07a7f2b62ea19e11620de992971d31e37cefc08
倾书 <nmlixa@163.com>
2018-07-09 00:09:37 +0800

    debug

--- Tools/TermsTools/1jdSortHeader.sh
+++ Tools/TermsTools/1jdSortHeader.sh
@@ -10,7 +10,10 @@ sort -t $'\t' -k 2 cizu.txt > xkjd6.cizu.dict.yaml
 sort -t $'\t' -k 2 danzi.txt > xkjd6.danzi.dict.yaml
 sort -t $'\t' -k 2 yingwen.txt > xkjd6.yingwen.dict.yaml
 sort -t $'\t' -k 2 wxw.txt > xkjd6.wxw.dict.yaml
-sort -t $'\t' -k 2 buchong.txt > xkjd6.buchong.dict.yaml
+# 补充码表不可重新排序 sort -t $'\t' -k 2 buchong.txt > xkjd6.buchong.dict.yaml
+cp -rf buchong.txt ./xkjd6.buchong.dict.yaml
+
+cp -rf xkjd6.buchong.dict.yaml ./buchong.txt
 cp -rf xkjd6.cizu.dict.yaml ./cizu.txt
 cp -rf xkjd6.danzi.dict.yaml ./danzi.txt
 cp -rf xkjd6.yingwen.dict.yaml ./yingwen.txt
@@ -21,7 +24,7 @@ sed -i 's/\b并不比\b\tbbb\b/---\nname: xkjd6.cizu\nversion: "Q1"\nsort: origi
 sed -i 's/\b不\b\tb\b/---\nname: xkjd6.danzi\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.danzi.dict.yaml
 sed -i 's/\bbiangbiang面\b\tbbmmu\b/---\nname: xkjd6.yingwen\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.yingwen.dict.yaml
 sed -i 's/\不能\b\tba\b/---\nname: xkjd6.wxw\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.wxw.dict.yaml
-sed -i 's/\乛\b\ta\b/---\nname: xkjd6.buchong\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.buchong.dict.yaml
+sed -i 's/\那\b\ta\b/---\nname: xkjd6.buchong\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.buchong.dict.yaml
 echo 执行排序添加头部信息完成
 cp -rf xkjd6.cizu.dict.yaml ../../rime/
 cp -rf xkjd6.danzi.dict.yaml ../../rime/
