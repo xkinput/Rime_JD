7b47ac83b60f8b84b617722ef74d4250adb5368e
qshu <nmlixa@163.com>
2018-07-22 21:07:54 +0800

    更新：据仪表内容更新词库

--- Tools/TermsTools/1jdSortHeader.sh
+++ Tools/TermsTools/1jdSortHeader.sh
@@ -3,6 +3,7 @@ cp -rf ../../rime/xkjd6.danzi.dict.yaml ../BackDict/
 cp -rf ../../rime/xkjd6.yingwen.dict.yaml ../BackDict/
 cp -rf ../../rime/xkjd6.wxw.dict.yaml ../BackDict/
 cp -rf ../../rime/xkjd6.buchong.dict.yaml ../BackDict/
+cp -rf ../../rime/xkjd6.chaojizici.dict.yaml ../BackDict/
 git add ../BackDict/*
 git commit -m "更新：备份旧词库"
 echo 开始处理排序
@@ -10,6 +11,7 @@ sort -t $'\t' -k 2 cizu.txt > xkjd6.cizu.dict.yaml
 sort -t $'\t' -k 2 danzi.txt > xkjd6.danzi.dict.yaml
 sort -t $'\t' -k 2 yingwen.txt > xkjd6.yingwen.dict.yaml
 sort -t $'\t' -k 2 wxw.txt > xkjd6.wxw.dict.yaml
+sort -t $'\t' -k 2 chaojizici.txt > xkjd6.chaojizici.dict.yaml
 # 补充码表不可重新排序 sort -t $'\t' -k 2 buchong.txt > xkjd6.buchong.dict.yaml
 cp -rf buchong.txt ./xkjd6.buchong.dict.yaml
 
@@ -19,16 +21,22 @@ cp -rf xkjd6.danzi.dict.yaml ./danzi.txt
 cp -rf xkjd6.yingwen.dict.yaml ./yingwen.txt
 cp -rf xkjd6.wxw.dict.yaml ./wxw.txt
 cp -rf xkjd6.buchong.dict.yaml ./buchong.txt
+cp -rf xkjd6.chaojizici.dict.yaml ./chaojizici.txt
 echo 更为当前文件
 sed -i 's/\b并不比\b\tbbb\b/---\nname: xkjd6.cizu\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.cizu.dict.yaml
 sed -i 's/\b不\b\tb\b/---\nname: xkjd6.danzi\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.danzi.dict.yaml
 sed -i 's/\bbiangbiang面\b\tbbmmu\b/---\nname: xkjd6.yingwen\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.yingwen.dict.yaml
 sed -i 's/\不能\b\tba\b/---\nname: xkjd6.wxw\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.wxw.dict.yaml
 sed -i 's/\那\b\ta\b/---\nname: xkjd6.buchong\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.buchong.dict.yaml
+sed -i 's/\那\b\ta\b/---\nname: xkjd6.chaojizici\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.chaojizici.dict.yaml
 echo 执行排序添加头部信息完成
 cp -rf xkjd6.cizu.dict.yaml ../../rime/
 cp -rf xkjd6.danzi.dict.yaml ../../rime/
 cp -rf xkjd6.yingwen.dict.yaml ../../rime/
 cp -rf xkjd6.wxw.dict.yaml ../../rime/
 cp -rf xkjd6.buchong.dict.yaml ../../rime/
+cp -rf xkjd6.chaojizici.dict.yaml ../../rime/
 echo 已放置到主词库中
+git add . ../../rime/*
+git commit -m "更新：据仪表内容更新词库"
+git push
