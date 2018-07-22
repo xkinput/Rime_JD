2af2bf9177c50d6aa99d932203cd12e42593e1b8
qshu <nmlixa@163.com>
2018-07-22 21:22:18 +0800

    更新：据仪表内容更新词库

--- Tools/TermsTools/1jdSortHeader.sh
+++ Tools/TermsTools/1jdSortHeader.sh
@@ -23,13 +23,13 @@ cp -rf xkjd6.wxw.dict.yaml ./wxw.txt
 cp -rf xkjd6.buchong.dict.yaml ./buchong.txt
 cp -rf xkjd6.chaojizici.dict.yaml ./chaojizici.txt
 echo 更为当前文件
-sed -i 's/\b并不比\b\tbbb\b/---\nname: xkjd6.cizu\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.cizu.dict.yaml
-sed -i 's/\b不\b\tb\b/---\nname: xkjd6.danzi\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.danzi.dict.yaml
-sed -i 's/\bbiangbiang面\b\tbbmmu\b/---\nname: xkjd6.yingwen\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.yingwen.dict.yaml
-sed -i 's/\不能\b\tba\b/---\nname: xkjd6.wxw\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.wxw.dict.yaml
-sed -i 's/\那\b\ta\b/---\nname: xkjd6.buchong\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.buchong.dict.yaml
-sed -i 's/\那\b\ta\b/---\nname: xkjd6.chaojizici\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.chaojizici.dict.yaml
-sed -i 's/\BiangBiang面\b\tbbmmu\b/---\nname: xkjd6.yingwen\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.yingwen.dict.yaml
+sed -i '1 i\n---\nname: xkjd6.cizu\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.cizu.dict.yaml
+sed -i '1 i\n---\nname: xkjd6.danzi\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.danzi.dict.yaml
+sed -i '1 i\n---\nname: xkjd6.yingwen\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.yingwen.dict.yaml
+sed -i '1 i\n---\nname: xkjd6.wxw\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.wxw.dict.yaml
+sed -i '1 i\n---\nname: xkjd6.buchong\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.buchong.dict.yaml
+sed -i '1 i\n---\nname: xkjd6.chaojizici\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.chaojizici.dict.yaml
+sed -i '1 i\n---\nname: xkjd6.yingwen\nversion: "Q1"\nsort: original\n...\n&/' xkjd6.yingwen.dict.yaml
 echo 执行排序添加头部信息完成
 cp -rf xkjd6.cizu.dict.yaml ../../rime/
 cp -rf xkjd6.danzi.dict.yaml ../../rime/
