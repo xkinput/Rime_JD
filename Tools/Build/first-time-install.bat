b9d1280c6468e4e06b4beaae4a1b8cdcd496cbef
不书 <nmlixa@163.com>
2018-11-24 10:31:48 +0800

    纠正：windows部署工具重选目录的首次安装工具

--- Tools/Build/first-time-install.bat
+++ Tools/Build/first-time-install.bat
@@ -6,8 +6,8 @@ echo 1、安装小狼毫输入法与小狼毫输入法扩展程序
 echo 注意：
 echo 一、请不要改动安装地址！
 echo 二、请选择稍后重启！
-"D:\Program Files\Rime_JD\Tools\SystemTools\WindowsTools\app\weasel-0.12.0.0-installer.exe"
-"D:\Program Files\Rime_JD\Tools\SystemTools\WindowsTools\app\weasel-expansion-0.9.18.0.exe"
+"Tools\SystemTools\WindowsTools\app\weasel-0.12.0.0-installer.exe"
+"Tools\SystemTools\WindowsTools\app\weasel-expansion-0.9.18.0.exe"
 echo .
 echo ======================
 echo 2、复制码表，并自动部署
@@ -19,12 +19,12 @@ echo.
 echo 获取最新资料完成
 echo.
 
-if exist "D:\Program Files\Rime_JD\Tools\SystemTools\WindowsTools\备份\" (
-  del "D:\Program Files\Rime_JD\Tools\SystemTools\WindowsTools\备份\" /S /Q
+if exist "Tools\SystemTools\WindowsTools\备份\" (
+  del "Tools\SystemTools\WindowsTools\备份\" /S /Q
 ) else (
-  mkdir "D:\Program Files\Rime_JD\Tools\SystemTools\WindowsTools\备份\"
+  mkdir "Tools\SystemTools\WindowsTools\备份\"
 )
-xcopy "%APPDATA%\Rime" "D:\Program Files\Rime_JD\Tools\SystemTools\WindowsTools\备份\" /Y /E
+xcopy "%APPDATA%\Rime" "Tools\SystemTools\WindowsTools\备份\" /Y /E
 cls
 echo 备份原有词库		完成
 
@@ -36,39 +36,39 @@ echo 复制码表文件		完成
 rmdir "%APPDATA%\Rime\Windows" /S /Q
 echo 删除冗余文件		完成
 
-xcopy "D:\Program Files\Rime_JD\Tools\SystemTools\rime\Windows\*" "%APPDATA%\Rime\" /Y /E
+xcopy "Tools\SystemTools\rime\Windows\*" "%APPDATA%\Rime\" /Y /E
 echo 复制独有配置		完成
 
 cls
 
-if exist "D:\Program Files\Rime_JD\Tools\SystemTools\WindowsTools\备份\xkjd6.user.dict.yaml" (
-  xcopy "D:\Program Files\Rime_JD\Tools\SystemTools\WindowsTools\备份\xkjd6.user.dict.yaml" "%APPDATA%\Rime\" /Y /E
+if exist "Tools\SystemTools\WindowsTools\备份\xkjd6.user.dict.yaml" (
+  xcopy "Tools\SystemTools\WindowsTools\备份\xkjd6.user.dict.yaml" "%APPDATA%\Rime\" /Y /E
   echo 用户词库还原完成
 ) else (
   echo 没有用户词库跳过
 )
-if exist "D:\Program Files\Rime_JD\Tools\SystemTools\WindowsTools\备份\xkjd6.extended.dict.yaml" (
-  xcopy "D:\Program Files\Rime_JD\Tools\SystemTools\WindowsTools\备份\xkjd6.extended.dict.yaml" "%APPDATA%\Rime\" /Y /E
+if exist "Tools\SystemTools\WindowsTools\备份\xkjd6.extended.dict.yaml" (
+  xcopy "Tools\SystemTools\WindowsTools\备份\xkjd6.extended.dict.yaml" "%APPDATA%\Rime\" /Y /E
   echo 扩展配置还原完成
 ) else (
   echo 没有扩展配置跳过
 )
-if exist "D:\Program Files\Rime_JD\Tools\SystemTools\WindowsTools\备份\xkjd6dz.extended.dict.yaml" (
-  xcopy "D:\Program Files\Rime_JD\Tools\SystemTools\WindowsTools\备份\xkjd6dz.extended.dict.yaml" "%APPDATA%\Rime\" /Y /E
+if exist "Tools\SystemTools\WindowsTools\备份\xkjd6dz.extended.dict.yaml" (
+  xcopy "Tools\SystemTools\WindowsTools\备份\xkjd6dz.extended.dict.yaml" "%APPDATA%\Rime\" /Y /E
   echo 单字扩展配置还原完成
 ) else (
   echo 没有单字扩展配置跳过
 )
 echo 还原用户配置		完成
 
-if exist "D:\Program Files\Rime_JD\Tools\SystemTools\WindowsTools\用户数据\" (
-  xcopy "D:\Program Files\Rime_JD\Tools\SystemTools\WindowsTools\用户数据\*" "%APPDATA%\Rime\" /Y /E
+if exist "Tools\SystemTools\WindowsTools\用户数据\" (
+  xcopy "Tools\SystemTools\WindowsTools\用户数据\*" "%APPDATA%\Rime\" /Y /E
   echo 还原用户数据		完成
 ) else (
-  mkdir "D:\Program Files\Rime_JD\Tools\SystemTools\WindowsTools\用户数据\"
+  mkdir "Tools\SystemTools\WindowsTools\用户数据\"
 )
 
-type D:\Program Files\Rime_JD\Tools\SystemTools\rime\Windows\_*.txt
+type "Tools\SystemTools\rime\Windows\_*.txt"
 echo.
 echo.
 
