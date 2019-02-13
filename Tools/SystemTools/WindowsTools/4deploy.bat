2074f36a5a4646a7e3b066e666a378f8ba463892
不书 <nmlixa@163.com>
2019-02-13 16:31:19 +0800

    修复兼容小狼毫0.12.0与0.13.0部署问题

--- Tools/SystemTools/WindowsTools/4deploy.bat
+++ Tools/SystemTools/WindowsTools/4deploy.bat
@@ -5,9 +5,25 @@ title 小狼毫重新部署工具
 
 echo 正在尝试自动部署！
 echo 请稍等...
+
+if exist "C:\Program Files (x86)\Rime\weasel-0.12.0\WeaselDeployer.exe" (
 "C:\Program Files (x86)\Rime\weasel-0.12.0\WeaselDeployer.exe" /deploy
+)
+
+if exist "C:\Program Files (x86)\Rime\weasel-0.13.0\WeaselDeployer.exe" (
+"C:\Program Files (x86)\Rime\weasel-0.13.0\WeaselDeployer.exe" /deploy
+)
+
 cls
 echo 部署完成！
 echo 更新完成，可以关闭我了
+
+if exist "C:\Program Files (x86)\Rime\weasel-0.12.0\WeaselServer.exe" (
 "C:\Program Files (x86)\Rime\weasel-0.12.0\WeaselServer.exe"
+)
+
+if exist "C:\Program Files (x86)\Rime\weasel-0.13.0\WeaselServer.exe" (
+"C:\Program Files (x86)\Rime\weasel-0.13.0\WeaselServer.exe"
+)
+
 exit
