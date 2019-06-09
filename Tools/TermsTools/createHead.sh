25f9559c56db304c6289f281086c882c08c75b55
nshu <nmlixa@163.com>
2019-06-10 00:05:55 +0800

    调整 创建头工具

--- Tools/TermsTools/createHead.sh
+++ Tools/TermsTools/createHead.sh
@@ -80,12 +80,11 @@ echo 已放置到主词库中
 
 git add . ../../rime/*
 
-read -p "请输入更新词库依据: [默认为议表]" upgradeOrigin
-
+read -p "请输入更新依据: [默认：据议表更新词库]" upgradeOrigin
 if [ -z $upgradeOrigin ];then
   git commit -m "更新：据议表更新词库"
 else
-  git commit -m "更新：据$upgradeOrigin提供词组更新词库"
+  git commit -m "更新：$upgradeOrigin更新词库"
 fi
 
 git push origin master
