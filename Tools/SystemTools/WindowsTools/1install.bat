c116f5c89f23c0f3c3f35e576148ea0e4546b9cd
nshu <nmlixa@163.com>
2019-06-15 18:38:03 +0800

    调整小狼毫脚本版本

--- Tools/SystemTools/WindowsTools/1install.bat
+++ Tools/SystemTools/WindowsTools/1install.bat
@@ -6,10 +6,10 @@ echo 1、安装小狼毫输入法与小狼毫输入法扩展程序
 echo 注意：
 echo 一、请不要改动安装地址！
 echo 二、请选择稍后重启！
-"%CD%\app\weasel-0.13.0.0-installer.exe"
+"%CD%\app\weasel-0.14.0.135-installer.exe"
 "%CD%\app\weasel-expansion-0.9.18.0.exe"
 echo .
 echo ======================
 echo 2、复制码表，并自动部署
-start %CD%\2update.bat
+"%CD%\2update.bat"
 exit
