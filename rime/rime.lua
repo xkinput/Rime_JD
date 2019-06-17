68bca9bb16b6087f1f70429d998542f6109a3494
nshu <nmlixa@163.com>
2019-06-17 22:32:09 +0800

    添加：支持日期时间 lua脚本，orq输出日期 oej 输出时间

---
+++ rime/rime.lua
@@ -0,0 +1,38 @@
+--[[
+librime-lua 样例
+```
+  engine:
+    translators:
+      - lua_translator@lua_function3
+      - lua_translator@lua_function4
+    filters:
+      - lua_filter@lua_function1
+      - lua_filter@lua_function2
+```
+其中各 `lua_function` 为在本文件所定义变量名。
+--]]
+
+--[[
+本文件的后面是若干个例子，按照由简单到复杂的顺序示例了 librime-lua 的用法。
+每个例子都被组织在 `lua` 目录下的单独文件中，打开对应文件可看到实现和注解。
+
+各例可使用 `require` 引入。
+```
+  foo = require("bar")
+```
+可认为是载入 `lua/bar.lua` 中的例子，并起名为 `foo`。
+配方文件中的引用方法为：`...@foo`。
+--]]
+
+-- date_translator: 将 `date` 翻译为当前日期
+-- 详见 `lua/date.lua`:
+date_translator = require("date")
+
+-- time_translator: 将 `time` 翻译为当前时间
+-- 详见 `lua/time.lua`
+time_translator = require("time")
+
+
+-- single_char_filter: 候选项重排序，使单字优先
+-- 详见 `lua/single_char.lua`
+single_char_filter = require("single_char")
