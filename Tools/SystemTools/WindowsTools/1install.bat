42a770d0331965b4955d443f88a9e0031865c1ed
nshu <nmlixa@163.com>
2019-09-26 19:56:23 +0800

    更新工具：windows脚本1install添加git ull

--- Tools/SystemTools/WindowsTools/1install.bat
+++ Tools/SystemTools/WindowsTools/1install.bat
@@ -6,6 +6,7 @@ echo 1、安装小狼毫输入法与小狼毫输入法扩展程序
 echo 注意：
 echo 一、请不要改动安装地址！
 echo 二、请选择稍后重启！
+git pull origin master
 "%CD%\app\weasel-0.14.3.0-installer.exe"
 "%CD%\app\weasel-expansion-0.9.18.0.exe"
 echo .
