abebed7084104d2a5d76314744622c5b10706eb3
real <nmlixa@163.com>
2020-07-28 15:13:40 +0800

    去除生成sbb提示文件

--- Tools/TermsTools/createHead.sh
+++ Tools/TermsTools/createHead.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 注意：使用CRLF行尾序列
+
 cizu=xkjd6.cizu.dict.yaml
 danzi=xkjd6.danzi.dict.yaml
 yingwen=xkjd6.yingwen.dict.yaml
@@ -6,17 +8,6 @@ wxw=xkjd6.wxw.dict.yaml
 chaojizici=xkjd6.chaojizici.dict.yaml
 buchong=xkjd6.buchong.dict.yaml
 lianjie=xkjd6.lianjie.dict.yaml
-
-cp -rf ../../rime/$cizu ../BackDict/
-cp -rf ../../rime/$danzi ../BackDict/
-cp -rf ../../rime/$yingwen ../BackDict/
-cp -rf ../../rime/$wxw ../BackDict/
-cp -rf ../../rime/$chaojizici ../BackDict/
-cp -rf ../../rime/$buchong ../BackDict/
-cp -rf ../../rime/$lianjie ../BackDict/
-
-git add ../BackDict/*
-git commit -m "更新：备份旧词库"
 
 echo 清理旧排序文件
 rm $cizu
@@ -36,12 +27,7 @@ cp -rf chaojizici.txt ./$chaojizici
 cp -rf buchong.txt ./$buchong
 cp -rf lianjie.txt ./$lianjie
 
-cp -rf $cizu ../../rime/opencc/WXWPromptFilters.txt
-echo 更为630简码提示词库
-
 if [ $(uname) == "Darwin" ];then
-
-    sed -i '' -E $'s///' ./*.txt
     sed -i '' -E $'1 i\\\n---\\\nname: xkjd6.cizu\\\nversion: "Q1"\\\nsort: original\\\n...\\\n' $cizu
     sed -i '' -E $'1 i\\\n---\\\nname: xkjd6.danzi\\\nversion: "Q1"\\\nsort: original\\\n...\\\n' $danzi
     sed -i '' -E $'1 i\\\n---\\\nname: xkjd6.yingwen\\\nversion: "Q1"\\\nsort: original\\\n...\\\n' $yingwen
@@ -49,15 +35,6 @@ if [ $(uname) == "Darwin" ];then
     sed -i '' -E $'1 i\\\n---\\\nname: xkjd6.buchong\\\nversion: "Q1"\\\nsort: original\\\n...\\\n' $buchong
     sed -i '' -E $'1 i\\\n---\\\nname: xkjd6.chaojizici\\\nversion: "Q1"\\\nsort: original\\\n...\\\n' $chaojizici
     sed -i '' -E $'1 i\\\n---\\\nname: xkjd6.lianjie\\\nversion: "Q1"\\\nsort: original\\\n...\\\n' $lianjie
-    sed -i '' -E $'s///' ../../rime/*.yaml
-
-    if [[ (-x "$(command -v python3)") && (-f "make_wxw_json.py") ]];then
-	python3 make_wxw_json.py
-        echo "630 词组提示更新完毕"
-    else
-        echo "630 词组提示尚未更新"
-    fi
-
 else
     sed -i '1 i---\nname: xkjd6.cizu\nversion: "Q1"\nsort: original\n...' $cizu
     sed -i '1 i---\nname: xkjd6.danzi\nversion: "Q1"\nsort: original\n...' $danzi
