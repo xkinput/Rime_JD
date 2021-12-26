04d57cb4863209c5bf9aefb9d01ee3a635b31b45
Xuesong Peng <pengxuesong.cn@gmail.com>
2021-12-26 09:49:41 +0800

    顶功提示与630提示共用开关

--- rime/lua/xkjd6_filter.lua
+++ rime/lua/xkjd6_filter.lua
@@ -35,7 +35,7 @@ local function filter(input, env)
     local is_on = env.engine.context:get_option('sbb_hint')
     local first = true
     local input_text = env.engine.context.input
-    local no_commit = input_text:len() < 4 and input_text:match("^[bcdefghjklmnpqrstwxyz]+$")
+    local no_commit = is_on and input_text:len() < 4 and input_text:match("^[bcdefghjklmnpqrstwxyz]+$")
     for cand in input:iter() do
         if first and no_commit and cand.type ~= 'completion' then
             commit_hint(cand)
