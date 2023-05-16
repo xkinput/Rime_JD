73fc575d4869f41e989c8c799250adcb22a04cc9
Xuesong Peng <pengxuesong.cn@gmail.com>
2023-05-16 14:30:49 +0800

    单引号作用于当前候选页面

--- rime/lua/smart_2.lua
+++ rime/lua/smart_2.lua
@@ -4,15 +4,19 @@ local kAccepted = 1 -- consume it
 local kNoop     = 2 -- leave it to other processors
 
 local function processor(key_event, env)
-    local engine = env.engine
-    local schema = engine.schema
-    local context = engine.context
-
+    -- 尽早返回以节省开销
     if key_event:release() or key_event:repr() ~= KEY then
         return kNoop
     end
 
-    if context:select(1) then
+    local engine = env.engine
+    local schema = engine.schema
+    local context = engine.context
+    local page_size = schema.page_size
+    local selected_index = context.composition:back().selected_index
+    local page_start = (selected_index / page_size) * page_size
+
+    if context:select(page_start + 1) then
         context:commit()
         return kAccepted
     end
